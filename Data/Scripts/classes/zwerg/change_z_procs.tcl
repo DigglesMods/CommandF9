460e570d8d7563792c42ecd68bff53d44dadf437
Christian Voß <cech12@gmail.com>
2020-05-11 09:32:07 +0200

    support weapon combinations #2 and use game intern weapon choose method to identify the best weapon

--- Data/Scripts/classes/zwerg/change_z_procs.tcl
+++ Data/Scripts/classes/zwerg/change_z_procs.tcl
@@ -13,16 +13,6 @@ proc beamto_world_all {} {
 	set myTrue 1
 	set myFalse 0
 	
-	//weapons
-	set ballistItem -1
-	set shieldItem -1
-	set onehandItem -1
-	set twohandItem -1
-	set weaponRankingBallist {Buechse PfeilUndBogen Steinschleuder}
-	set weaponRankingShield {Trollschild_3 Drachenschuppe Schild_unq_2 Schild_unq_1 Trollschild_2 Kristallschild Schild_3 Metallschild Schild_2 Schild_1 Trollschild_1 Schild}
-	set weaponRankingOneHand {Axt_unq_3 Schwert_4 Krumsaebel Axt_3 Streitkolben Schwert Dolch_1 Axt_unq_1 Schwert_1 Axt_1 Dolch_2 Keule}
-	set weaponRankingTwoHand {Hellebarde Axt_unq_4 Zauberstab Axt_4 Lichtschwert Schwert_3 Axt_unq_2 Lanze_2 Axt_2 Schwert_2 Streitaxt Lanze_1}
-	
 	set searchlockclasses [list]
 	//add tools
 	if {$my$print:TOOLS} {
@@ -36,17 +26,10 @@ proc beamto_world_all {} {
 	if {$my$print:TRANSPORT} {
 		lappend searchlockclasses Holzkiepe Grosse_Holzkiepe Schubkarren
 	}
-	//add weapons
-	if {$my$print:WEAPONS} {
-		foreach item $weaponRankingBallist {lappend searchlockclasses $item}
-		foreach item $weaponRankingShield {lappend searchlockclasses $item}
-		foreach item $weaponRankingOneHand {lappend searchlockclasses $item}
-		foreach item $weaponRankingTwoHand {lappend searchlockclasses $item}
-	}
 	
 	set lockclasses [list]
 	set rememberItems [list]
-
+	
 	//search and remember items
 	foreach item [inv_list this] {
 		set objclass [get_objclass $item]
@@ -68,20 +51,6 @@ proc beamto_world_all {} {
 						if {[inv_find this Grosse_Holzkiepe]  > -1} {
 							set canAppend 0
 						}
-					} elseif {$my$print:WEAPONS} {
-						if {[lsearch $weaponRankingBallist $objclass]  > -1} {
-							set canAppend 0
-							set ballistItem [get_better_weapon $weaponRankingBallist $ballistItem $item]
-						} elseif {[lsearch $weaponRankingShield $objclass]  > -1} {
-							set canAppend 0
-							set shieldItem [get_better_weapon $weaponRankingShield $shieldItem $item]
-						} elseif {[lsearch $weaponRankingOneHand $objclass]  > -1} {
-							set canAppend 0
-							set onehandItem [get_better_weapon $weaponRankingOneHand $onehandItem $item]
-						} elseif {[lsearch $weaponRankingTwoHand $objclass]  > -1} {
-							set canAppend 0
-							set twohandItem [get_better_weapon $weaponRankingTwoHand $twohandItem $item]
-						}
 					}
 				}
 				
@@ -95,12 +64,35 @@ proc beamto_world_all {} {
 			}
 		}
 	}
-	
-	//remember weapons
-	if {$ballistItem > -1} {lappend rememberItems $ballistItem}
-	if {$shieldItem > -1} {lappend rememberItems $shieldItem}
-	if {$onehandItem > -1} {lappend rememberItems $onehandItem}
-	if {$twohandItem > -1} {lappend rememberItems $twohandItem}
+
+	//add weapons
+	if {$my$print:WEAPONS} {
+		set bestBallistic [get_best_weapon this 1]
+		set ballistic [lindex $bestBallistic 0]
+		if {$ballistic != -1} {
+			lappend rememberItems $ballistic
+		}
+		set bestWeaponShield [get_best_weapon this 0]
+		set weapon [lindex $bestWeaponShield 0]
+		if {$weapon != -1} {
+			lappend rememberItems $weapon
+			//keep fitting amulet
+			set amuletPos -1
+			switch [get_objclass $weapon] {
+				Axt_unq_2 { set amuletPos [inv_find this Amulett_1] }
+				Axt_unq_4 { set amuletPos [inv_find this Amulett_2] }
+				Schwert_2 { set amuletPos [inv_find this Amulett_3] }
+				default {}
+			}
+			if {$amuletPos != -1} {
+				lappend rememberItems [inv_get this $amuletPos]
+			}
+		}
+		set shield [lindex $bestWeaponShield 1]
+		if {$shield != -1} {
+			lappend rememberItems $shield
+		}
+	}
 	
 	//drop all items
 	foreach item [inv_list this] {
@@ -114,52 +106,6 @@ proc beamto_world_all {} {
 		take_item $item
 	}
 	
-}
-
-proc is_weapon_usable {weapon} {
-        set widtrue [get_weapon_id $weapon true]
-		set widfalse [get_weapon_id $weapon false]
-		set weaponID 0
-		if {$widtrue > $widfalse} {set weaponID $widtrue} else {set weaponID $widfalse}
-		return [check_weapon_exp this $weaponID]
-}
-
-proc get_better_weapon {weaponRanking weap1 weap2} {
-	if {$weap1 == -1} {
-		set weap1 $weap2
-	} elseif {$weap2 == -1} {
-		set weap2 $weap1
-	}
-	
-	//check dwarf experience
-	set weap1_exp [is_weapon_usable $weap1]
-	set weap2_exp [is_weapon_usable $weap2]
-	
-	if {$weap1_exp && $weap2_exp} {
-		//both weapons can be used
-		//check witch one is better
-		set objclass1 [get_objclass $weap1]
-		set objclass2 [get_objclass $weap2]
-		
-		foreach objclass $weaponRanking {
-			if {$objclass == $objclass1} {
-				return $weap1
-			}
-			if {$objclass == $objclass2} {
-				return $weap2
-			}
-		}
-	} else {
-		//only one can be used
-		if {$weap1_exp} {
-			return $weap1
-		}
-		if {$weap2_exp} {
-			return $weap2
-		}
-	}
-	// no one can be used
-	return -1
 }
 $end
 
